d550e03c6a6e3af6659add629b251921329afb47
Leslie A. Lopez <lesashley56@gmail.com>
2017-02-22 13:53:25 -0500

    convertidor

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+function convertirCelsius(){
+  var valor = document.getElementById('farenheit').value;
+  document.getElementById("celsius").value = (valor * (9 / 5) + 32);
+}
+function convertirFarenheit(){
+  var valor = document.getElementById("celsius").value;
+  document.getElementById("farenheit").value = (valor - 32) * (5/9);
+}
+function limpiar() {
+document.getElementById("celsius").value="";
+document.getElementById("farenheit").value="";
+}
